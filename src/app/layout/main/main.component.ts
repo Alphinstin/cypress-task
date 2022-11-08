c961f84b4ae0895ae57fe2af9c29677ce1f32856
Alphinstin <alpholi32@gmail.com>
2022-11-08 21:09:44 +0100

    Project setup

---
+++ src/app/layout/main/main.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss'],
+})
+export class MainComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
